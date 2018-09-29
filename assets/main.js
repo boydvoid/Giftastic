541407daf59eed3b01b4327f8d57d6ce132f1919
Bobby Boyd <bboyd2008@gmail.com>
2018-09-29 00:52:32 -0700

    toggle gif or static

--- assets/main.js
+++ assets/main.js
@@ -5,6 +5,8 @@ $(document).ready(() => {
   let api = "9MEVd7MXtXTlO2ZtbNMF0JKMMuUQOEmo";
   let btnRow = $("#buttons-row");
   let giphyURL = "https://api.giphy.com/v1/gifs/search?api_key=" + api + "&";
+  let statics = [];
+  let gifs = [];
   let twitterURL;
 
   //append the topic buttons to the html
@@ -49,7 +51,11 @@ $(document).ready(() => {
         //create the image div
         let img = $("<img>");
         img.attr("src", res.data[i].images.original_still.url);
+        statics.push(res.data[i].images.original_still.url);
+        gifs.push(res.data[i].images.original.url);
         img.attr("class", "static image");
+        img.attr("data-index", [i]);
+        img.attr("id", [i]);
         //create the column
         let col = $("<div>");
         col.attr("class", "col-xl-3");
@@ -59,12 +65,23 @@ $(document).ready(() => {
     });
   }
 
-  //images onclick 
-  $(document).on('click', '.static', function () {
-    
-
-
-  })
+  //images onclick
+  $(document).on("click", ".static", function() {
+    if (
+      $(this)
+        .attr("class")
+        .includes("gif")
+    ) {
+      let image = $(this).attr("data-index");
+      $(this).attr("src", statics[parseInt(image)]);
+      $(this).removeClass("gif");
+    } else {
+      console.log(this);
+      let gif = $(this).attr("data-index");
+      $(this).attr("src", gifs[parseInt(gif)]);
+      $(this).addClass("gif");
+    }
+  });
 
   // end of doc ready
 });
